4cedf55c0088bf6b331e8a9f4e3eedbb97b7d0cd
hanh0722 <78199926+hanh0722@users.noreply.github.com>
2021-05-15 12:27:14 +0700

    change

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const app = express();
 const db = Knex({
     client: 'pg',
         connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-        }
+        host: process.env.DATABASE_URL,
+        ssl: true
     }
 })
 
